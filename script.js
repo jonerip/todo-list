fbf08675e4717ed0ffb36351a13903f09b93f423
unknown <joneripaz@gmail.com>
2025-03-10 02:53:15 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,45 @@
+const num = 5;
+const ul = document.getElementById("addedTasks");
+
+function addTask() {
+  if (ul.children.length >= num) {
+    alert("Max Tasks Reached!");
+    return;
+
+  } else {
+    const task = document.getElementById("newTask").value;
+    const task2 = task.charAt(0).toUpperCase() + task.slice(1);
+    if (task2 === "") {
+      alert("Error! Cannot be Empty!");
+      return;
+    }
+
+    const li = document.createElement("li");
+    li.textContent = task2;
+    ul.appendChild(li);
+
+    const button = document.createElement("button");
+    button.textContent = "Delete";
+    button.style.borderRadius = "8px";
+    button.style.backgroundColor = "rgb(233, 63, 63)";
+    li.appendChild(button);
+    button.onclick = () => {
+      li.remove();
+    };
+
+    document.getElementById("newTask").value = "";
+  }
+}
+
+function delTask() {
+  //ul.lastElementChild.remove();
+  //ul.remove();
+  //ul.innerHTML = "";
+  ul.replaceChildren();
+  
+  /*if (ul.children.length == 0) {
+    true;
+    return;
+  } 
+    */
+}
